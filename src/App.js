98016d881b295c1cd7f0ace6fabf52b9b52d85a2
nadiasalsaa <tugasnadia28@gmail.com>
2021-06-22 08:47:32 +0700

    Halaman Feed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Feeds from './Pages/Feeds';
+import MyProfile from './Pages/MyProfile';
+// import FProfile from './Pages/FProfile ';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+    render() {
+      return (
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/">
+                  <Feeds />
+              </Route>
+              <Route path="/MyProfile">
+                  <MyProfile />
+              </Route>
+              {/* <Route path="/FProfile">
+                  <FProfile />
+              </Route> */}
+          </Switch>
+          </div>
+        </Router>
+      );
+    }
 }
 
 export default App;
