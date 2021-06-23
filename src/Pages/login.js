5e4789ca41a83baf985f4f2cf113f184e2fd593b
feriyalbinyahya <feriyalbinyahya@gmail.com>
2021-06-23 08:50:29 +0700

    Login Page

---
+++ src/Pages/login.js
@@ -0,0 +1,40 @@
+import {Component} from 'react';
+import './login.css';
+import AlertDialogRegister from '../components/AlertDialogRegister';
+import AlertDialogNewPost from '../components/AlertDialogNewPost';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <div className="content">
+                    <div className="title-app">
+                        <h1 className="skyter-title">Skyter</h1>
+                        <p className="skyter-desc">Skyter membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda</p>
+                    </div>
+                    <div className="form-login-box">
+                        <form action="" className="form-login">
+                            <input type="email" name="email" placeholder="Email" className="input-login input-email" />
+                            <input type="password" name="password" id="" placeholder="Kata Sandi" className="input-login input-password" />
+                            <input type="submit" value="Login" className="submit-login-btn" />
+                        </form>
+                        <p style={{fontSize: 12}}>atau</p>
+                        <button className="btn-facebook">Login Facebook <i class="fa fa-facebook-f"></i></button><br />
+                        <button className="btn-google">Login Google <i class="fa fa-google"></i></button>
+                        <hr className="line-form" />
+                        <AlertDialogRegister />
+                        <AlertDialogNewPost />
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
