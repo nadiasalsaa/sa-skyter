f29cf70708dfc5fedc60c4fb79d1d0c23cdc9de7
feriyalbinyahya <firriyal.bin@ui.ac.id>
2021-07-17 15:38:14 +0700

    on Click untuk masing masing button

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,11 +1,28 @@
 import {Component} from 'react';
 import './asset/login.css';
-import AlertDialogRegister from '../component/AlertDialogRegister';
-import AlertDialogNewPost from '../component/AlertDialogNewPost';
+import AlertDialogRegister from '../components/AlertDialogRegister';
+import AlertDialogNewPost from '../components/AlertDialogNewPost';
 
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data_login: {
+                email: '',
+                password: '',
+            },
+        }
+    }
+
+    submitLogin = (email, password) => {
+        alert("email: " + email + ", password: " + password);
+    }
+
+    handleChangeDataLogin = (event) => {
+        const { value, name } = event.target;
+        const { data_login } = this.state;
+        data_login[name] = value;
+        this.setState({ data_login: data_login});
     }
 
     render() {
@@ -18,13 +35,13 @@ class Login extends Component {
                     </div>
                     <div className="form-login-box">
                         <form action="" className="form-login">
-                            <input type="email" name="email" placeholder="Email" className="input-login input-email" />
-                            <input type="password" name="password" id="" placeholder="Kata Sandi" className="input-login input-password" />
-                            <input type="submit" value="Login" className="submit-login-btn" />
+                            <input type="email" name="email" placeholder="Email" className="input-login input-email" onChange={this.handleChangeDataLogin} />
+                            <input type="password" name="password" id="" placeholder="Kata Sandi" className="input-login input-password" onChange={this.handleChangeDataLogin} />
+                            <input type="submit" value="Masuk" className="submit-login-btn" onClick={() => this.submitLogin(this.state.data_login.email, this.state.data_login.password)} />
                         </form>
                         <p style={{fontSize: 12}}>atau</p>
-                        <button className="btn-facebook">Login Facebook <i class="fa fa-facebook-f"></i></button><br />
-                        <button className="btn-google">Login Google <i class="fa fa-google"></i></button>
+                        <button className="btn-facebook">Masuk Facebook <i class="fa fa-facebook-f"></i></button><br />
+                        <button className="btn-google">Masuk Google <i class="fa fa-google"></i></button>
                         <hr className="line-form" />
                         <AlertDialogRegister />
                         <AlertDialogNewPost />
--- src/components/AlertDialogEditProfile.js
+++ src/components/AlertDialogEditProfile.js
@@ -11,6 +11,11 @@ import './AlertDialogEditProfile.css';
 
 export default function AlertDialogEditProfile() {
   const [open, setOpen] = React.useState(false);
+  const [data_profil, setDataProfil] = React.useState({
+    img: "",
+    nama: "",
+    deskripsi: "",
+  });
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -20,34 +25,45 @@ export default function AlertDialogEditProfile() {
     setOpen(false);
   };
 
+  const handleChangeDataProfil = (event) => {
+    const { value, id } = event.target;
+    setDataProfil({...data_profil, [id]:value});
+  }
+
+  const submitProfil = () => {
+    alert("image: " + data_profil.img + ", nama: " + data_profil.nama + ", deskripsi: " + data_profil.deskripsi);
+  }
+
   return (
     <div>
-      <button className="akun-baru-button" variant="contained" color="primary" onClick={handleClickOpen}>
+      <button className="edit-profil-button" variant="contained" color="primary" onClick={handleClickOpen}>
         Edit Profile
       </button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <ThemeProvider>
           <p id="form-dialog-title" className="form-dialog-title">Ubah Profil</p>
           <DialogContent>
-          <input type="file" id="img" name="img" accept="image/*" value=""/>
+          <input type="file" id="img" name="img" accept="image/*" value="" onChange={handleChangeDataProfil}/>
           <TextField
               margin="dense"
-              id="status"
+              id="nama"
               label=""
               variant="outlined"
-              value="Andika Amri"
+              placeholder="Nama"
               type="textarea"
+              onChange={handleChangeDataProfil}
               inputProps={{style: {fontFamily: `'Poppins', sans-serif`, width: 150}}} // font size of input text
               InputLabelProps={{style: {fontFamily: `'Poppins', sans-serif`}}} // font size of input label
               fullWidth
             />
             <TextField
               margin="dense"
-              id="status"
+              id="deskripsi"
               label=""
               variant="outlined"
               placeholder="Deskripsi"
               type="textarea"
+              onChange={handleChangeDataProfil}
               inputProps={{style: {fontFamily: `'Poppins', sans-serif`, width: 320}}} // font size of input text
               InputLabelProps={{style: {fontFamily: `'Poppins', sans-serif`}}} // font size of input label
               fullWidth
@@ -59,7 +75,7 @@ export default function AlertDialogEditProfile() {
             <button onClick={handleClose} className="cancel-button" color="primary">
               Batal
             </button>
-            <button onClick={handleClose} className="post-button" color="primary">
+            <button onClick={() => submitProfil()} className="post-button" color="primary">
               Ubah Profil
             </button>
           </DialogActions>
